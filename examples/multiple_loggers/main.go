73bd5e5b237ce556cdcfcf50d544c1d1e1b6258e
J. Santos <jamillo@gmail.com>
2019-01-03 01:12:24 -0300

    Adds a multiple logger example

---
+++ examples/multiple_loggers/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/lab259/rlog"
+	"os"
+)
+
+func main() {
+	stdOutLogger, err := rlog.NewLogger(rlog.Config{})
+	if err != nil {
+		panic(err)
+	}
+	stdOutLogger.SetOutput(os.Stdout)
+	stdErrLogger, err := rlog.NewLogger(rlog.Config{})
+	if err != nil {
+		panic(err)
+	}
+	stdErrLogger.SetOutput(os.Stderr)
+	stdOutLogger.Info("This log line goes to the STDOUT")
+	stdErrLogger.Info("This log line goes to the STDERR")
+}
